6f42b00c66a73109a90e8fa270afa0ed3dee6c04
“Olga <lolgaaleksandrovna93@gmail.com>
2026-08-04 01:49:03 +0300

    feat: refactor Radio class with Lombok and fix plugin versions

--- src/test/java/com/example/smarthome/RadioTest.java
+++ src/test/java/com/example/smarthome/RadioTest.java
@@ -7,7 +7,7 @@ class RadioTest {
 
     @Test
     void nextStationFrom0To8() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentStation(5);
         radio.next();
         assertEquals(6, radio.getCurrentStation());
@@ -15,7 +15,7 @@ class RadioTest {
 
     @Test
     void nextStationFrom9To0() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentStation(9);
         radio.next();
         assertEquals(0, radio.getCurrentStation());
@@ -23,7 +23,7 @@ class RadioTest {
 
     @Test
     void prevStationFrom9To8() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentStation(9);
         radio.prev();
         assertEquals(8, radio.getCurrentStation());
@@ -31,7 +31,7 @@ class RadioTest {
 
     @Test
     void prevStationFrom0To9() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentStation(0);
         radio.prev();
         assertEquals(9, radio.getCurrentStation());
@@ -39,28 +39,28 @@ class RadioTest {
 
     @Test
     void setStationValid() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentStation(3);
         assertEquals(3, radio.getCurrentStation());
     }
 
     @Test
     void setStationInvalidTooLow() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentStation(-1);
-        assertEquals(0, radio.getCurrentStation()); // остаётся на дефолтном
+        assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     void setStationInvalidTooHigh() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentStation(10);
-        assertEquals(0, radio.getCurrentStation()); // остаётся на дефолтном
+        assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     void increaseVolumeFrom0To50() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentVolume(50);
         radio.increaseVolume();
         assertEquals(51, radio.getCurrentVolume());
@@ -68,7 +68,7 @@ class RadioTest {
 
     @Test
     void increaseVolumeAtMax() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentVolume(100);
         radio.increaseVolume();
         assertEquals(100, radio.getCurrentVolume());
@@ -76,7 +76,7 @@ class RadioTest {
 
     @Test
     void decreaseVolumeFrom100To50() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentVolume(50);
         radio.decreaseVolume();
         assertEquals(49, radio.getCurrentVolume());
@@ -84,7 +84,7 @@ class RadioTest {
 
     @Test
     void decreaseVolumeAtMin() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentVolume(0);
         radio.decreaseVolume();
         assertEquals(0, radio.getCurrentVolume());
@@ -92,21 +92,21 @@ class RadioTest {
 
     @Test
     void setVolumeValid() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentVolume(75);
         assertEquals(75, radio.getCurrentVolume());
     }
 
     @Test
     void setVolumeInvalidTooLow() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentVolume(-5);
         assertEquals(0, radio.getCurrentVolume());
     }
 
     @Test
     void setVolumeInvalidTooHigh() {
-        Radio radio = new Radio();
+        Radio radio = new Radio(10); // Было: new Radio()
         radio.setCurrentVolume(105);
         assertEquals(0, radio.getCurrentVolume());
     }
